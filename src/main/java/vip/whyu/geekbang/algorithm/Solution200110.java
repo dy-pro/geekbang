6407a636b8bcf223331aa78de82d9e80c5f1ae0a
lsucre <lsucre@163.com>
2020-01-11 00:07:28 +0800

    first commit

---
+++ src/main/java/vip/whyu/geekbang/algorithm/Solution200110.java
@@ -0,0 +1,25 @@
+package vip.whyu.geekbang.algorithm;
+
+/**
+ * @Description: 极客大学-算法小课-01-每日一课
+ * @Company: whyu.vip
+ * @ProjectName: geekbang
+ * @Author: Lsucre
+ * @GmtCreate: 2020-01-10 23:06:28
+ */
+public class Solution200110 {
+    /**
+     * 每日一题 : 可思考，可争论，要五毒。
+     * 今日五毒操练 《remove-duplicates-from-sorted-array》
+     * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+     *
+     * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     *
+     * 示例 1: 给定数组 nums = [1,1,2],
+     * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+     *
+     * 示例 2: 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+     * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+     */
+}
---
+++ src/test/java/vip/whyu/geekbang/algorithm/Solution200110Tests.java
@@ -0,0 +1,16 @@
+package vip.whyu.geekbang.algorithm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Solution200110Tests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
